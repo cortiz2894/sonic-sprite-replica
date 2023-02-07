646283245b369680c05d80cb9589881c6e6f3297
Christian Daniel Ortiz Ororbia <cortiz2894@gmail.com>
2023-02-07 16:44:47 -0300

    add legs correction and left hand

--- src/components/Sonic.js
+++ src/components/Sonic.js
@@ -3,6 +3,26 @@ import styles from './Sonic.module.scss'
 const Sonic = () => {
     return(
         <div className={styles.root}>
+            <div className={styles.arms}>
+                <div className={styles.left}>
+                    <div className={styles.arm}>
+                        <div className={styles.inner}></div>
+                    </div>
+                    <div className={styles.hand}>
+                        <div className={styles.leftShadows}></div>
+                        <div className={styles.glove}>
+                            <div className={styles.inner}></div>
+                        </div>
+                        <div className={styles.rightShadows}>
+                            <div className={styles.inner}></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.arm}></div>
+                    <div className={styles.hand}></div>
+                </div>
+            </div>
             <div className={styles.boots}>
                 <div className={styles.left}>
                     <div className={styles.leg}></div>
@@ -20,9 +40,18 @@ const Sonic = () => {
                     </div>
                 </div>
                 <div className={styles.right}>
-                    <div className={styles.socks}></div>
-                    <div className={styles.laces}></div>
-                    <div className={styles.bottom}></div>
+                    <div className={styles.leg}></div>
+                    <div className={styles.socks}>
+                        <div className={styles.inner}></div>
+                    </div>
+                    <div className={styles.laces}>
+                        <div className={styles.upperRed}></div>
+                        <div className={styles.inner}></div>
+                        <div className={styles.inner2}></div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <div className={styles.inner}></div>
+                    </div>
                 </div>
             </div>
         </div>
