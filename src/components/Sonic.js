2b13bd6e2d469c574b6742ae1642d203ad4a8a9f
Christian Daniel Ortiz Ororbia <cortiz2894@gmail.com>
2023-02-09 23:58:01 -0300

    half head finish

--- src/components/Sonic.js
+++ src/components/Sonic.js
@@ -3,6 +3,27 @@ import styles from './Sonic.module.scss'
 const Sonic = () => {
     return(
         <div className={styles.root}>
+            <div className={styles.head}>
+                <div className={styles.hair}>
+                    <div className={styles.hears}>
+                        <div className={styles.left}></div>
+                        <div className={styles.right}></div>
+                    </div>
+                    <div className={styles.eyes}>
+                        <div className={styles.left}></div>
+                        <div className={styles.right}></div>
+                    </div>
+                    <div className={styles.thorn1}></div>
+                    <div className={styles.thorn2}><div></div></div>
+                    <div className={styles.thorn3}></div>
+                </div>
+                <div className={styles.mouth}>
+                    <div className={styles.glow}></div>
+                    <div className={styles.nose}></div>
+                    <div className={styles.smile}></div>
+                    <div className={styles.borderBottom}></div>
+                </div>
+            </div>
             <div className={styles.arms}>
                 <div className={styles.left}>
                     <div className={styles.arm}>
@@ -18,9 +39,28 @@ const Sonic = () => {
                         </div>
                     </div>
                 </div>
+                <div className={styles.body}>
+                    <div className={styles.chest}>
+                        <div className={styles.light}>
+                            <div></div>
+                        </div>
+                        <div className={styles.shadow}>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
                 <div className={styles.right}>
                     <div className={styles.arm}></div>
-                    <div className={styles.hand}></div>
+                    <div className={styles.hand}>
+                        <div className={styles.glove}>
+                            <div className={styles.inner}></div>
+                            <div className={styles.shadows}>
+                                <div className={styles.innerShadow}>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={styles.boots}>
